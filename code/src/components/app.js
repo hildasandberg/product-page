b56c64fd12852a7aca9408f8f52c4a4d932790e5
Hilda <hilda.sandberg@gmail.com>
2017-12-04 19:25:34 +0100

    cleaning

--- code/src/components/app.js
+++ code/src/components/app.js
@@ -3,8 +3,6 @@ import Page from "./page"
 import "../index.css"
 const productsJson = require("./../products.json")
 
-// console.log("Products: (Remove this line in app.js!)", productsJson)
-
 class App extends React.Component {
 
   render() {
